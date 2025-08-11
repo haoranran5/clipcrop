d3d7f9329b01c56e93dad1b522a17ab5b286bb3a
haoranran5 <123869495+haoranran5@users.noreply.github.com>
2025-08-11 11:39:39 +0800

    0.71

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import LanguageRouter from './components/LanguageRouter'
+import App from './App'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <LanguageRouter />
+    <App />
   </React.StrictMode>,
 )
 
